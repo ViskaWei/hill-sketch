a234bf34f2ffec73af33460548d84c0653eedaa1
ViskaWei <swei20@jhu.edu>
2020-11-24 19:40:07 -0500

    fix csv file

--- hillSimple/hillSimple.py
+++ hillSimple/hillSimple.py
@@ -14,10 +14,15 @@ from scipy.spatial.distance import cdist
 ########################### Loading #######################################
 
 def load_data(DATASET):
-    data = pd.read_excel(DATASET)    
+    if DATASET[-3:]=='csv':
+        data = pd.read_csv(DATASET)    
+    elif DATASET[-4:]=='xlsx':
+        data = pd.read_excel(DATASET)    
+    else:
+        raise 'can only read csv or xlsx file'
     keep_columns = data.columns[(data.sum()!=0)]
     print(keep_columns)
-    return data[keep_columns],keep_columns   
+    return data[keep_columns], keep_columns 
 
 ########################### Preprocessing #######################################
 
